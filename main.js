6a24cd6e69edde9448a7d5ab9de1d76361c8dbc7
Karen Polanco <polanco.k@gmail.com>
2022-12-11 18:32:13 -0500

    commit tree 329fa4be74f3e9ff3264b708044f37aeef4a0557

--- main.js
+++ main.js
@@ -1,8 +1,79 @@
+/*in my own words
+ when user clicks a heart, miniServerCall() should be activated. 
+miniServerCall() will either return a success or a fail
+success = heart becomes red
+1. add .activated-heart to make the empty heart have a red outline
+2. also use const FULL_HEART to make it completely red
+(do this inside the of the .then)
+fail = error banner appears 
+1. use then to return the error banner
+2. use catch & timeOut to have the banner disappear after 3 seconds
+
+other tasks
+When the page loads, error banner shouldn't pop up immedately
+When a red heart is clicked again, remove activated heart and include const EMPTY_HEART
+ */
+
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+
+//Gobal Scope. Snagged stuff
+let errorModal = document.querySelector("#modal")
+let hearts = document.querySelectorAll(".like-glyph")
+//console.log(hearts)
+errorModal.classList.add("hidden")
+
+//When the page loads, error banner shouldn't pop up immedately
+
+function hideErrorModal(){
+  errorModal.classList.add("hidden")
+} 
+hideErrorModal()
+
+//click on a heart
+
+function likeListener(heart){
+  hearts.forEach(heart => {
+    heart.addEventListener("click", e => {
+      let clickedHeart = e.target
+      //console.log(clickedHeart.innerText, "vs;", EMPTY_HEART)
+      //console.log("clickedHeart; ", clickedHeart)
+      mimicServerCall()
+      .then((res) => {
+        if (clickedHeart.innerText === EMPTY_HEART) {
+          clickedHeart.classList.add("activated-heart")
+          clickedHeart.innerText = FULL_HEART
+        } else {
+          clickedHeart.classList.remove("activated-heart")
+          clickedHeart.innerText = EMPTY_HEART
+        }
+      })
+      .catch((error) => {
+        errorModal.classList.remove("hidden")
+        setTimeout(hideErrorModal, 3000)
+      })
+    })
+  })
+}
+
+likeListener()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
